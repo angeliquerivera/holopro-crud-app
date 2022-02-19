721359b10a3036cc2516790680ec9571f26b15dc
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-02-18 21:01:02 -0500

    added talents folder and new.ejs

--- routes/talentsRouter.js
+++ routes/talentsRouter.js
@@ -5,8 +5,14 @@ const talentsRouter = express.Router();
  * Handles all /talents routes
  */
 
+// /talents
 talentsRouter.get("/", (req, res) => {
   res.send("On talents' index page.");
+});
+
+// /talents/new
+talentsRouter.get("/new", (req, res) => {
+  res.render("talents/new");
 });
 
 module.exports = talentsRouter;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/talents", talentsRouter);
  * Server index route
  */
 app.get("/", (req, res) => {
-  res.render("index", { talents });
+  res.render("talents/index", { talents });
 });
 
 /**
